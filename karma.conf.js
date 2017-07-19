d35a7e61e5b12403e4986c994e92d5db62393ace
Wayne Rumble <rumblewayne@gmail.com>
2017-07-19 15:29:23 +0100

    fixed npm

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
-    port: process.env.PORT || 3000,
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@
 export const environment = {
   production: false,
   token_auth_config: {
-    apiBase: 'http://localhost:3000'
+    apiBase: process.env.PORT || 'http://localhost:3000'
   }
 };
